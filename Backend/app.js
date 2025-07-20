04a9ad1207dbf06c470119ae5b9280a3d0e998c6
Vikas Singh Kushwaha <129819852+vi1ka2@users.noreply.github.com>
2025-07-20 05:49:59 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
